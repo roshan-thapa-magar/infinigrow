e3b9b6b6783d79ebd4d3ba67df334b1241accbd2
Roshan Thapa Magar <roshan@octspace.com>
2026-09-03 19:42:32 +0545

    language

--- components/contact/contact-hero.tsx
+++ components/contact/contact-hero.tsx
@@ -42,7 +42,7 @@ export default function ContactPage() {
 
       {/* Dialog */}
       <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
-        <DialogContent className="!max-w-3xl max-h-[100vh] overflow-y-auto p-0 z-[9999]">
+        <DialogContent className="!max-w-3xl max-h-[98vh] overflow-y-auto p-0 z-[9999]">
           <ProjectRequestForm  />
         </DialogContent>
       </Dialog>
